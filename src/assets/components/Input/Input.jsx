50fe80ffd7bee63e99c657f3e1c33a380d2ac8b1
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-08-26 16:21:23 +0600

    add delete butoon

--- src/assets/components/Input/Input.jsx
+++ src/assets/components/Input/Input.jsx
@@ -32,6 +32,10 @@ const Input = () => {
     );
   };
 
+  const deleteItem = (id) => {
+    setNotes((notes) => notes.filter((note) => note.id !== id));
+  };
+
   return (
     <>
       <div className=" w-full mx-auto p-4 border border-gray-300 rounded-lg shadow-md max-w-md">
@@ -54,24 +58,39 @@ const Input = () => {
           </div>
         </div>
       </div>
-      <div className=" ">
+      <div className=" flex justify-center mt-5 gap-1.5">
         {Array.isArray(notes) && notes.length > 0 ? (
           notes.map((note) => (
-            <div key={note.id}>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-accent"
-                onChange={() => toggleComplete(note.id)}
-                id={note.id}
-                checked={!!note.complete}
-                readOnly={false}
-              />{' '}
-              <label
-                htmlFor={note.id}
-                className={note.complete ? 'line-through text-gray-500' : ''}
-              >
-                {note.text}
-              </label>
+            <div
+              key={note.id}
+              className=" border-2 p-3 rounded-xl bg-yellow-200 border-green-500"
+            >
+              <div className=" flex gap-2 justify-center align-middle items-center">
+                <input
+                  type="checkbox"
+                  className="checkbox checkbox-accent"
+                  onChange={() => toggleComplete(note.id)}
+                  id={note.id}
+                  checked={!!note.complete}
+                  readOnly={false}
+                />{' '}
+                <label
+                  htmlFor={note.id}
+                  className={
+                    note.complete
+                      ? 'line-through text-gray-500  text-xl font-medium'
+                      : ' text-xl font-medium'
+                  }
+                >
+                  {note.text}
+                </label>
+                <button
+                  className="btn btn-error cursor-pointer"
+                  onClick={() => deleteItem(note.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           ))
         ) : (
